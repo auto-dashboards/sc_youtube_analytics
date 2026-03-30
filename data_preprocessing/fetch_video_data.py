61030905d25fcad295336005346530070ffacdcf
Adnan1237 <adnan1237@googlemail.com>
2026-03-30 19:40:43 +0100

    add docs string

--- data_preprocessing/fetch_video_data.py
+++ data_preprocessing/fetch_video_data.py
@@ -19,6 +19,26 @@ def fetch_video_ids():
 
 def fetch_video_data(video_ids, youtube_api):
 
+    '''
+    Fetches video data for a list of Youtube Videos and returns as a Pandas Dataframe. I.e. Video metadata, video statistics etc 
+
+    This function: 
+    1. Splits the video list into batches of 50 (maximum allowed as input for the Youtube API)
+    2. Fetches metadata for each batch using the Youtube Data API
+    3. Stores each videos data as a JSON string in a Dataframe 
+    4. Extracts the video ID into a separate column for each row 
+
+    Args: 
+        video_ids (list): List of Youtube video IDs 
+        youtube_api: Authenticated Youtube API client for connection 
+
+    Returns: 
+        Dataframe with columns:
+            video_data: JSON string containing full video metadata
+            video_id: Youtube video ID
+
+    '''
+
     # Batch video requests (50 IDs per call) and call fields
     def batch_video(lst, n):
         for i in range(0, len(lst), n):
@@ -42,6 +62,28 @@ def fetch_video_data(video_ids, youtube_api):
 
 
 def fetch_video_min_data(video_ids, analytics_api):
+
+    '''
+    Fetches video minute data for a list of Youtube videos and returns as a Pandas Dataframe. 
+    I.e. for every 60 seconds, what are the average viewers and peak viewers
+
+    This is separated from fetch_video_data() as you need to retrieve this data from the analytics api as opposed to data api
+      
+    This function: 
+    1. For every video id, retrieve the average viewers and peak viewers per 60 seconds
+    2. Stores each videos data as a JSON string in a Dataframe 
+    3. Extracts the video ID into a separate column for each row 
+    4. Concat them into a single dataframe
+
+    Args: 
+        video_ids (list): List of Youtube video IDs 
+        analytics_api: Authenticated Youtube API client for connection 
+
+    Returns: 
+        Dataframe with columns:
+            video_id: Youtube video ID
+            minute_metrics: JSON string containing full video metadata
+    '''
 
     start_date = '2022-09-16'
     end_date = date.today().isoformat()
@@ -84,6 +126,25 @@ def fetch_video_min_data(video_ids, analytics_api):
 
 def fetch_video_est_watched(video_ids, analytics_api):
 
+    '''
+    Fetches video estimated watch time for a list of Youtube videos and returns as a Pandas Dataframe. 
+
+    This is separated from fetch_video_data() as you need to retrieve this data from the analytics api as opposed to data api
+    This is separated from fetch_video_min_data() as the dimensions field needs to be different and can't be joined together
+      
+    This function: 
+    1. For every video id, retrieve the estimated watch time (mins)
+
+    Args: 
+        video_ids (list): List of Youtube video IDs 
+        analytics_api: Authenticated Youtube API client for connection 
+
+    Returns: 
+        Dataframe with columns:
+            video_id: Youtube video ID
+            estimatedMinutesWatched: Estimated total watch time (mins)
+    '''
+
     start_date = '2022-09-16'  # YouTube's earliest possible date
     end_date = date.today().isoformat()
 
@@ -115,7 +176,7 @@ def fetch_video_est_watched(video_ids, analytics_api):
 
 if __name__ == "__main__":
 
-    # Load from .env file
+    # === Load environment variables from .env file ===
     load_dotenv()
     api_key = os.getenv('YI_API_KEY')
     channel_id = os.getenv('CHANNEL_KEY')
@@ -125,20 +186,26 @@ if __name__ == "__main__":
     dbl_url = os.environ['DBL_URL']
     print('Loaded environment variables')
 
+    # === Fetch youtube video IDs ===
     video_ids = fetch_video_ids()
     print('Fetched video IDs')
+
+    # === Connect to the youtube data and youtube analytics API's ===
     youtube_api = connect_yt_data_api(api_key)
-    analytics_api = connect_yt_analytics_api(api_key, refresh_token, client_id, client_secret)
+    analytics_api = connect_yt_analytics_api(refresh_token, client_id, client_secret)
     print('Connected to Youtube Data & Analytics APIs')
 
+    # === Fetch youtube video metrics ===
     video_data = fetch_video_data(video_ids, youtube_api)
     video_min_data = fetch_video_min_data(video_ids, analytics_api)
     video_est_watched = fetch_video_est_watched(video_ids, analytics_api)
     print('Fetched all Youtube video data')
 
+    # === Join video data into a single table ===
     video_metrics_comb = video_data.merge(video_min_data, left_on='video_id', right_on='video_id', how='left').merge(video_est_watched, left_on='video_id', right_on='video', how='left')
     video_metrics_comb = video_metrics_comb[['video_id', 'video_data', 'minute_metrics', 'estimatedMinutesWatched']]
 
+    # === Insert video data into postgreSQL ===
     insert_records_to_postgres(dbl_url, 'sc_yt_video_data', video_metrics_comb)
     print('Inserted records into Postgres')
 
--- data_preprocessing/utils/helper_functions.py
+++ data_preprocessing/utils/helper_functions.py
@@ -11,16 +11,38 @@ import io
 
 def connect_yt_data_api(api_key):
     '''
-    Returns a connection to the youtube data api
+    Creates a connection to the Youtube Data API
+    
+    Args: 
+        api_key (str): Your Youtube Data API key
+    
+    Returns: 
+        A youtube API client object
+
     '''
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     return youtube
 
 
-def connect_yt_analytics_api(api_key, refresh_token, client_id, client_secret):
+def connect_yt_analytics_api(refresh_token, client_id, client_secret):
     '''
-    Returns a connection to the youtube analytics api
+    Creates a connection to the Youtube Analytics API
+
+    Since the Youtube Analytics API requires OAuth2 authentication (i.e. asking the user to login and authorise access), 
+    to stop manual requests, a refresh token alongisde client info is required to request an access token each time we run this API.
+
+    None -> is a placeholder for the access token, but we will recieve this once we refresh
+
+    Args:
+        refresh_token (str): OAuth2 refresh token for the account
+        client_id (str): OAuth2 client ID
+        client_secret (str): OAuth2 client secret
+
+    Returns: 
+        A Youtube Analytics API client object   
+
+
     '''
 
     credentials = Credentials(
@@ -41,6 +63,17 @@ def get_channel_videos_ids(api_key, channel_id):
 
     '''
     Returns a list of all video IDs for a given youtube channel
+
+    The API returns at most 50 videos per request (a rule by Youtube). 
+    It starts with nextPageToken=None, which is referring to the first page, 
+    and after it extracts the first 50 videos, it takes the next page token for the second page and so on. 
+    Once it has extracted it all, next page token will be None to which the loop ends
+
+    Args: 
+        api_key (str): Your Youtube Data API key
+        channel_id (str): Your Youtube channel ID
+
+    
     '''
 
     youtube = connect_yt_data_api(api_key)
@@ -78,6 +111,22 @@ def get_channel_videos_ids(api_key, channel_id):
 
 
 def insert_records_to_postgres(dbl_url, pg_table_name, df):
+
+    '''
+    Inserts records from a Pandas Dataframe into a PostgreSQL staging table. 
+
+    This function performs a full refresh of the specified table: 
+    1. Connects to the database using the provided URL
+    2. Truncates the staging table to remove existing data
+    3. Copies the dataframe into the table using an in memory CSV buffer. More efficient as it saves on your RAM instead of disk. 
+
+    Args: 
+        dbl_url (str): PostgreSQL connection URL
+        pg_table_name (str): Name of the staging table to refresh 
+        df: Pandas dataframe containing data to insert into the staging table 
+    
+    
+    '''
 
     # === Connect to Neon DB on Postgres ===
     dbl_url = os.environ['DBL_URL']
